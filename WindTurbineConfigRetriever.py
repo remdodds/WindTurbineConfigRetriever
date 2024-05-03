99d0e428dc10b433d35b395ecfdfb6c4461591b8
remdodds <remdodds@googlemail.com>
2024-05-03 14:09:17 +0100

    Images pulled and saved to local file

--- WindTurbineConfigRetriever.py
+++ WindTurbineConfigRetriever.py
@@ -1,10 +1,23 @@
 import requests
 import time
+from pathlib import Path
+import urllib.request
 api_url = "https://script.google.com/macros/s/AKfycbwzXfRARanEuxoCskvrlvdpS6xLiDPwpBVfhRCMydUqCNaG5gsLhyD07oH4gHyeh0pS/exec"
 while True:
     response = requests.get(api_url)
     config = response.json()
     print(config)
+    imageUrlsList = config["imageURLs"]
+
+    #Set up the images folder
+    folderPath = Path("images")
+    folderPath.mkdir(exist_ok=True)
+    
+
+    for imageURL in imageUrlsList:
+        urllib.request.urlretrieve(imageURL[1], folderPath /  imageURL[0])
+    
+
     with open("WindTurbineConfig.txt", "w") as file:
-        file.write(config)
+        file.write(str(config))
     time.sleep(30)
