464d75560241a81f79d77ae4afe9f07c7a971be4
remdodds <remdodds@googlemail.com>
2024-05-03 17:32:49 +0100

    getting it to pull images and store them correctly

--- WindTurbineConfigRetriever.py
+++ WindTurbineConfigRetriever.py
@@ -13,10 +13,25 @@ while True:
     folderPath = Path("images")
     folderPath.mkdir(exist_ok=True)
     
-
-    for imageURL in imageUrlsList:
-        urllib.request.urlretrieve(imageURL[1], folderPath /  imageURL[0])
-    
+    # Get local images
+    localImageList = []
+    for file in folderPath.iterdir():
+        if file.is_file():
+            localImageList.append(file.name)
+    serverImageList = []
+    #Download images that are not already stored localy
+    for image in imageUrlsList:
+        imageFileName = image[0]
+        imageURL = image[1]
+        serverImageList.append(imageFileName)
+        if(imageFileName not in localImageList):
+            print("fetching " + imageFileName)
+            urllib.request.urlretrieve(imageURL, folderPath /  imageFileName)
+    #cleanup local images that are no longer in the google drive
+    for localImage in localImageList:
+        if(localImage not in serverImageList):
+            Path.unlink(folderPath / localImage)
+        
 
     with open("WindTurbineConfig.txt", "w") as file:
         file.write(str(config))
